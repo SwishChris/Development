b8a8995f494bf65b31d54780a2f73a031612eb6f
SwishChris <Junix@JUNIX-PC>
2018-01-25 02:41:38 -0600

    Assets

--- SkillsUSA Program/MainWindow.xaml.cs
+++ SkillsUSA Program/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
 
 namespace SkillsUSA_Program
 {
@@ -23,6 +26,39 @@ namespace SkillsUSA_Program
         public MainWindow()
         {
             InitializeComponent();
+        }
+        OpenFileDialog ofd = new OpenFileDialog();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            XDocument doc = new XDocument();
+            using (StreamReader streamReader = new StreamReader(ofd.FileName, true))
+            {
+                doc = XDocument.Load(streamReader);
+            }
+            doc.Root.Add(
+                new XElement("Name", txtbox.Text));
+            doc.Save(@"C:\Desktop.xml");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        { 
+            ofd.InitialDirectory = "C:\\";
+            ofd.Filter = "(*.xml)|*.xml|(*.*)|*.*";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            ofd.ShowDialog();
+
+            
         }
     }
 }
